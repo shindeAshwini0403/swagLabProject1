b3e09b45721923b9abafb48676e7e39cda9a39eb
sashw <sashw@DESKTOP-S2085P6>
2024-12-03 14:27:23 +0530

    I have created a new filter class and also automated a few test cases

---
+++ src/main/java/org/LoginModule/filterPage.java
@@ -0,0 +1,14 @@
+package org.LoginModule;
+
+import org.LibraryFiles.BaseClass;
+
+public class filterPage extends BaseClass
+{
+   public void m1()
+   {
+	   System.out.println("hello");
+   }
+	
+	}
+
+
