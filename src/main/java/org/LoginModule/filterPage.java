79242ee04659c4421680b1593d8e4cea36a2b824
sashw <sashw@DESKTOP-S2085P6>
2024-12-03 16:34:11 +0530

    I have make same changes in filter page

--- src/main/java/org/LoginModule/filterPage.java
+++ src/main/java/org/LoginModule/filterPage.java
@@ -4,7 +4,7 @@ import org.LibraryFiles.BaseClass;
 
 public class filterPage extends BaseClass
 {
-	//create new method
+   //create new changes
    public void m1()
    {
 	   System.out.println("hello");
